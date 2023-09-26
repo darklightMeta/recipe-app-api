ab912d552b532f0cb638bb9b9dc726bf6811b362
darklightMeta <133187489+darklightMeta@users.noreply.github.com>
2023-09-27 05:43:50 +0930

    lint correction test.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,17 +2,16 @@
 Sample tests
 """
 from django.test import SimpleTestCase
-
-from app import  cal
+from app import cal
 
 
 class TestCalc(SimpleTestCase):
     def test_add(self):
         res = cal.add(5, 6)
-
         self.assertEqual(res, 11)   # assertEqual is a method of SimpleTestCase
+
 
     def test_subtract(self):
         res = cal.subtract(5, 6)
-
         self.assertEqual(res, -1)
+
